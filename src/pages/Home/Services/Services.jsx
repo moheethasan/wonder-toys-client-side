e90e7d117f9b14da828abe56d4db638e5c9fa5a2
majesty-01 <moheethasan4@gmail.com>
2023-05-21 06:15:07 +0600

    added our services

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,5 +1,54 @@
+import {
+  FaMedal,
+  FaShoppingBag,
+  FaRegUserCircle,
+  FaShippingFast,
+} from "react-icons/fa";
+
 const Services = () => {
-  return <div>service</div>;
+  return (
+    <div className="container mx-auto px-4 py-16">
+      <h1 className="text-center text-4xl md:text-5xl font-bold pb-3 mt-12">
+        <span className="title-text">Our Services</span>
+      </h1>
+      <div className="grid grid-cols-4 gap-10 mt-12">
+        <div className="card bg-base-100 shadow-xl">
+          <figure className="text-5xl px-10 pt-10">
+            <FaMedal />
+          </figure>
+          <div className="card-body">
+            <h2 className="text-lg font-bold text-center">
+              Official Manufacturer Shop
+            </h2>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <figure className="text-5xl px-10 pt-10">
+            <FaShoppingBag />
+          </figure>
+          <div className="card-body">
+            <h2 className="text-lg font-bold text-center">Largest selection</h2>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <figure className="text-5xl px-10 pt-10">
+            <FaRegUserCircle />
+          </figure>
+          <div className="card-body">
+            <h2 className="text-lg font-bold text-center">Personal service</h2>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <figure className="text-5xl px-10 pt-10">
+            <FaShippingFast />
+          </figure>
+          <div className="card-body">
+            <h2 className="text-lg font-bold text-center">Fast delivery</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
